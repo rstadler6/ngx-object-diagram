d088d3fa9932ba3d512d77c0c0db0a9838870a4f
Benjamin Schäublin <be.schaeublin@gmail.com>
2023-04-01 15:12:47 +0200

    (chore): test

--- projects/ngx-object-diagram/src/lib/directives/svg-ellipsis.directive.ts
+++ projects/ngx-object-diagram/src/lib/directives/svg-ellipsis.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostBinding, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 const ELLIPSIS = '\u2026';
 
