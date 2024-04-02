fe19102864554ba6f2df965e905931f36a811571
Raphael Stadler <raphael.stadler@cmiag.ch>
2024-04-02 15:32:35 +0200

    fix(height): fix autoheight

--- projects/ngx-object-diagram/src/lib/components/ngx-object-diagram/ngx-object-diagram.component.ts
+++ projects/ngx-object-diagram/src/lib/components/ngx-object-diagram/ngx-object-diagram.component.ts
@@ -94,6 +94,9 @@ export class NgxObjectDiagramComponent implements OnInit, AfterViewChecked {
         this._entityWidth = parseInt(getComputedStyle(this._elementRef.nativeElement).getPropertyValue('--entity-min-width'), 10);
         this._initialHeight =
             parseInt(getComputedStyle(this._elementRef.nativeElement).getPropertyValue('--ngx-obj-diagram-height'), 10) || 800;
+        if (this.autoAdjustHeight) {
+            this._calculateHeight();
+        }
         this._calculatePositions();
     }
 
