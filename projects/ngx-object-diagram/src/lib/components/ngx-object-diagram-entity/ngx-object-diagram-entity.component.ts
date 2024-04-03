60c596911e75e669a4a4b5a1778639a00cecc63d
Raphael Stadler <raphael.stadler@cmiag.ch>
2024-04-03 15:45:47 +0200

    chore(cleanup): remove unused import

--- projects/ngx-object-diagram/src/lib/components/ngx-object-diagram-entity/ngx-object-diagram-entity.component.ts
+++ projects/ngx-object-diagram/src/lib/components/ngx-object-diagram-entity/ngx-object-diagram-entity.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ElementRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgxObjectDiagramEntityField } from '../../model/ngx-object-diagram-entity-field';
 @Component({
     selector: '[ngx-object-diagram-entity]',
